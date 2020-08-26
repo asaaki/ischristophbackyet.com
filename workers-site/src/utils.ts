f2b67af70591de017de3416d5851fdbf9277cb37
Christoph Grabo <asaaki@mannaz.cc>
2020-08-26 21:21:49 +0200

    Add missing docs for logHeaders

--- workers-site/src/utils.ts
+++ workers-site/src/utils.ts
@@ -1,3 +1,8 @@
+/**
+ * Print the header pairs for debugging purposes
+ * @param name - which response object are we logging?
+ * @param headers - the headers object of the response
+ */
 export function logHeaders(name: string, headers: Headers) {
   let pairs = Object.fromEntries(headers.entries());
   console.log(`[DEBUG] ${name}.headers:`);
